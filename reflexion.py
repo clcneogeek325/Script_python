87607c97fa738b3e64aefbe0e8c4425724ecff73
NEO <graff_rasmus@hotmail.com>
2016-06-03 22:26:44 -0500

    Reflexion

--- reflexion.py
+++ reflexion.py
@@ -1,1 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+class miclase():
+    def __init__(self):
+        self.version = '1.0'
+     
+    def suma(self,a, b):
+        return a+b
+          
+    def resta(self,a,b):
+        return a-b
+
+
+obj = miclase()
+
+# accediendo a los metodos de python de modo regular
+print obj.suma(4,5)
+print obj.resta(5,2)
+
+# accediendo a los metodos de python por medio de la reflexion
+print getattr(obj,'suma')(3,4)
+print getattr(obj,'resta')(7,2)
